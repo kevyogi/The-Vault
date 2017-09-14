38bd20dffb258e3633c0e3454b0f6c8c8bc5475e
Kevin Yogi <kevinyogi@Kevins-MacBook-Pro.local>
2017-09-13 15:09:32 -1000

    all tests passing

--- vault.js
+++ vault.js
@@ -1,4 +1,21 @@
 'use strict';
 module.exports = function() {
-  
+  function setValue (key, value){
+    setValue.key = key;
+    setValue.value = value;
+  }
+
+  function getValue(key){
+    if(key === undefined){
+      return null;
+    }else if(key !== setValue.key){
+      return null;
+    }else if(key === setValue.key){
+      return setValue.value;
+    }
+  }
+ return {
+  setValue: setValue,
+  getValue: getValue
+ }
 };
